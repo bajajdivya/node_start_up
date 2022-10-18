98cbb409a53782a0817a7e1424d168c8605a9164
bajajdivya <bajajdivya02@gmail.com>
2022-10-18 18:33:47 +0530

    first commit

---
+++ 1_intro.js
@@ -0,0 +1,8 @@
+const names = require("./names");
+const sayHi = require("./utils");
+const data = require('./alternative_flavor')
+require('./mind_grenade')
+// console.log(data)s
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
---
+++ mind_grenade.js
@@ -0,0 +1,6 @@
+const num1 = 50
+const num2 = 1000
+function addValues() {
+    console.log(`the sum is ${num1 + num2}`)
+}
+addValues()
